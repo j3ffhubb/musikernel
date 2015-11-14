d5bed23d378667d7082454197c3d7d2e427374ad
MusiKernel Team <musikernel@nospam.org>
2015-11-13 20:10:31 -0800

    Coding style cleanup

--- src/pydaw/libmodsynth/modules/dynamics/limiter.h
+++ src/pydaw/libmodsynth/modules/dynamics/limiter.h
@@ -61,20 +61,20 @@ void v_lim_free(t_lim_limiter * a_lim)
 void v_lim_set(t_lim_limiter*a_lim, float a_thresh, float a_ceiling,
         float a_release)
 {
-    if(a_thresh != (a_lim->last_thresh))
+    if(a_thresh != a_lim->last_thresh)
     {
-        a_lim->thresh = f_db_to_linear_fast((a_thresh));
-        a_lim->autogain = (1.0f/(a_lim->thresh));
+        a_lim->thresh = f_db_to_linear_fast(a_thresh);
+        a_lim->autogain = 1.0f / a_lim->thresh;
         a_lim->last_thresh = a_thresh;
     }
 
-    if(a_ceiling != (a_lim->last_ceiling))
+    if(a_ceiling != a_lim->last_ceiling)
     {
-        a_lim->volume = f_db_to_linear_fast((a_ceiling));
+        a_lim->volume = f_db_to_linear_fast(a_ceiling);
         a_lim->last_ceiling = a_ceiling;
     }
 
-    if(a_release != (a_lim->last_release))
+    if(a_release != a_lim->last_release)
     {
         if(a_release <= 0)
         {
@@ -86,15 +86,14 @@ void v_lim_set(t_lim_limiter*a_lim, float a_thresh, float a_ceiling,
         }
 
 
-        a_lim->r =
-                ((a_lim->sr_recip) / (a_lim->release)) * LMS_HOLD_TIME_DIVISOR;
+        a_lim->r = (a_lim->sr_recip / a_lim->release) * LMS_HOLD_TIME_DIVISOR;
         a_lim->last_release = a_release;
     }
 }
 
 void v_lim_run(t_lim_limiter *a_lim, float a_in0, float a_in1)
 {
-    a_lim->maxSpls = f_lms_max(f_lms_abs(a_in0),f_lms_abs(a_in1));
+    a_lim->maxSpls = f_lms_max(f_lms_abs(a_in0), f_lms_abs(a_in1));
 
     /*clip at 24dB.  If a memory error causes a huge value,
      * the limiter would never return.*/
@@ -105,7 +104,7 @@ void v_lim_run(t_lim_limiter *a_lim, float a_in0, float a_in1)
 
     ++a_lim->r1Timer;
 
-    if((a_lim->r1Timer) >= (a_lim->holdtime))
+    if(a_lim->r1Timer >= a_lim->holdtime)
     {
         a_lim->r1Timer = 0;
         a_lim->max1Block = (a_lim->max1Block) - (a_lim->r);
@@ -116,14 +115,13 @@ void v_lim_run(t_lim_limiter *a_lim, float a_in0, float a_in1)
         }
     }
 
-    a_lim->max1Block = f_lms_max((a_lim->max1Block), (a_lim->maxSpls));
-
+    a_lim->max1Block = f_lms_max(a_lim->max1Block, a_lim->maxSpls);
     ++a_lim->r2Timer;
 
-    if((a_lim->r2Timer) >= (a_lim->holdtime))
+    if(a_lim->r2Timer >= a_lim->holdtime)
     {
         a_lim->r2Timer = 0;
-        a_lim->max2Block = (a_lim->max2Block) - (a_lim->r);
+        a_lim->max2Block = a_lim->max2Block - a_lim->r;
 
         if((a_lim->max2Block) < 0.0f)
         {
@@ -131,34 +129,34 @@ void v_lim_run(t_lim_limiter *a_lim, float a_in0, float a_in1)
         }
     }
 
-    a_lim->max2Block = f_lms_max((a_lim->max2Block), (a_lim->maxSpls));
+    a_lim->max2Block = f_lms_max(a_lim->max2Block, a_lim->maxSpls);
 
-    a_lim->env = f_lms_max((a_lim->max1Block), (a_lim->max2Block));
+    a_lim->env = f_lms_max(a_lim->max1Block, a_lim->max2Block);
 
     if(a_lim->env > a_lim->thresh)
     {
-        a_lim->gain = ((a_lim->thresh) / (a_lim->env)) * (a_lim->volume);
+        a_lim->gain = (a_lim->thresh / a_lim->env) * a_lim->volume;
     }
     else
     {
-        a_lim->gain = (a_lim->volume);
+        a_lim->gain = a_lim->volume;
     }
 
-    a_lim->buffer0[(a_lim->buffer_index)] = a_in0;
-    a_lim->buffer1[(a_lim->buffer_index)] = a_in1;
+    a_lim->buffer0[a_lim->buffer_index] = a_in0;
+    a_lim->buffer1[a_lim->buffer_index] = a_in1;
 
     ++a_lim->buffer_index;
 
-    if((a_lim->buffer_index) >= (a_lim->buffer_size))
+    if(a_lim->buffer_index >= a_lim->buffer_size)
     {
         a_lim->buffer_index = 0;
     }
 
     float f_gain =
-        v_svf_run_4_pole_lp(&a_lim->filter, (a_lim->gain) * (a_lim->autogain));
+        v_svf_run_4_pole_lp(&a_lim->filter, a_lim->gain * a_lim->autogain);
 
-    a_lim->output0 = (a_lim->buffer0[(a_lim->buffer_index)]) * f_gain;
-    a_lim->output1 = (a_lim->buffer1[(a_lim->buffer_index)]) * f_gain;
+    a_lim->output0 = a_lim->buffer0[a_lim->buffer_index] * f_gain;
+    a_lim->output1 = a_lim->buffer1[a_lim->buffer_index] * f_gain;
 
     v_pkm_redux_run(&a_lim->peak_tracker, a_lim->gain);
 }
@@ -167,34 +165,34 @@ void g_lim_init(t_lim_limiter * f_result, float a_sr, int a_huge_pages)
 {
     f_result->holdtime = ((int)(a_sr / LMS_HOLD_TIME_DIVISOR));
 
-    f_result->buffer_size = (f_result->holdtime); // (int)(a_sr*0.003f);
+    f_result->buffer_size = f_result->holdtime; // (int)(a_sr*0.003f);
     f_result->buffer_index = 0;
 
     if(a_huge_pages)
     {
-        hpalloc((void**)&f_result->buffer0, sizeof(float) *
-            (f_result->buffer_size));
-        hpalloc((void**)&f_result->buffer1, sizeof(float) *
-            (f_result->buffer_size));
+        hpalloc((void**)&f_result->buffer0,
+            sizeof(float) * f_result->buffer_size);
+        hpalloc((void**)&f_result->buffer1,
+            sizeof(float) * f_result->buffer_size);
     }
     else
     {
-        lmalloc((void**)&f_result->buffer0, sizeof(float) *
-            (f_result->buffer_size));
-        lmalloc((void**)&f_result->buffer1, sizeof(float) *
-            (f_result->buffer_size));
+        lmalloc((void**)&f_result->buffer0,
+            sizeof(float) * f_result->buffer_size);
+        lmalloc((void**)&f_result->buffer1,
+            sizeof(float) * f_result->buffer_size);
     }
 
     int f_i;
 
-    for(f_i = 0; f_i < (f_result->buffer_size); f_i++)
+    for(f_i = 0; f_i < f_result->buffer_size; ++f_i)
     {
         f_result->buffer0[f_i] = 0.0f;
         f_result->buffer1[f_i] = 0.0f;
     }
 
     f_result->r1Timer = 0;
-    f_result->r2Timer = (f_result->holdtime)/2;
+    f_result->r2Timer = f_result->holdtime / 2;
 
     f_result->ceiling = 0.0f;
     f_result->env = 0.0f;
@@ -217,11 +215,9 @@ void g_lim_init(t_lim_limiter * f_result, float a_sr, int a_huge_pages)
     v_svf_set_cutoff_base(&f_result->filter, f_pit_hz_to_midi_note(4000.0f));
     v_svf_set_cutoff(&f_result->filter);
 
-    f_i = 0;
-    while(f_i < 50)
+    for(f_i = 0; f_i < 50; ++f_i)
     {
         v_svf_run_4_pole_lp(&f_result->filter, 1.0f);
-        ++f_i;
     }
 
     //nonsensical values that it won't evaluate to on the first run
