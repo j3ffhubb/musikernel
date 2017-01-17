a37c9eda4e0b5d488e34f6e62e4b1302b721e450
MusiKernel Team <musikernel@nospam.org>
2017-01-16 22:20:59 -0800

    Remove a package that's broken in Fedora 25
    It will only prevent creating the live image, which hasn't been
    distributed in a long time

--- src/fedora_deps.sh
+++ src/fedora_deps.sh
@@ -3,7 +3,8 @@ sudo dnf install python3-qt5 gcc alsa-lib-devel liblo-devel \
 libsndfile-devel gcc-c++ git python3-numpy \
 fftw-devel portmidi-devel portaudio-devel rubberband python3-devel \
 @development-tools fedora-packager livecd-tools spin-kickstarts \
-system-config-kickstart vorbis-tools gettext
+vorbis-tools gettext
+#system-config-kickstart
 
 # Now run:
 #
